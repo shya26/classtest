23b17199f1abc95a64f87955783dc49c3475dfdd
shya26 <shreyatrapasiya5@gmail.com>
2021-05-15 22:12:33 +0530

    first commit

---
+++ src/main/java/org/example/calculator10.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.openqa.selenium.By;
+
+public class calculator10 {
+    public void UserShouldBeToChangeCurrencySuccessfully(){
+        ClickOnElement(By.cssSelector("Euro"));
+    }
+
+    private void ClickOnElement(By euro) {
+    }
+}
